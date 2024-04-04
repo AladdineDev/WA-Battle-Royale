452929cee314da8913775d105e8584d080688fd6
BadPioche <dwillemain@hotmail.fr>
2024-04-04 12:44:50 +0200

    feature works in local, have to now implement it to work at the same time for all players

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,117 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { Tile } from "./Entity/Tile";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
+let timeCounter = 300; // 5 minutes
+let map: any = undefined;
+const mapConfig = { height: 0, width: 0 };
+const numberTileLimit = 3;
+let tic = 0;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+	.then(async () => {
+		console.log("Scripting API ready");
+		console.log("Player tags: ", WA.player.tags);
+		map = await WA.room.getTiledMap();
+		console.log(map);
+		mapConfig.height = map.height ?? 0;
+		mapConfig.width = map.width ?? 0;
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+		initTimerGame(timeCounter);
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+		let movementConfig = true;
+		let playersConfig = true;
+		await WA.players.configureTracking({
+			players: playersConfig,
+			movement: movementConfig,
+		});
+		console.log(
+			`Player config tracking ok -> movement: ${movementConfig}, players: ${playersConfig}`
+		);
+		const players = WA.players.list();
+		for (const player of players) {
+			console.log(`Player ${player.name} is near you`);
+		}
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+		WA.room.area.onEnter("clock").subscribe(() => {
+			const today = new Date();
+			const time = today.getHours() + ":" + today.getMinutes();
+			currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+		});
 
-}).catch(e => console.error(e));
+		WA.room.area.onLeave("clock").subscribe(closePopup);
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+		// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+		bootstrapExtra()
+			.then(() => {
+				console.log("Scripting API Extra ready");
+			})
+			.catch((e) => console.error(e));
+	})
+	.catch((e) => console.error(e));
+
+function closePopup() {
+	if (currentPopup !== undefined) {
+		currentPopup.close();
+		currentPopup = undefined;
+	}
+}
+
+function initTimerGame(timeCounter: number) {
+	setInterval(() => {
+		timeCounter -= 10;
+		if (timeCounter <= 300) {
+			handleTileRemovedByTime();
+		}
+		numberTileLimit;
+	}, 10000); // 10 seconds
+}
+
+function handleTileRemovedByTime() {
+	const tileToModify = [];
+	for (let i = 0; i < mapConfig.height; i++) {
+		const line = [];
+		for (let j = 0; j < mapConfig.width; j++) {
+			if (
+				i == tic ||
+				i == mapConfig.height - tic ||
+				j == tic ||
+				j == mapConfig.width - tic
+			) {
+				if (isNotInSafeZone(i, j, numberTileLimit)) {
+					line.push(new Tile(j, i, "uglyBlue", "above/laptops"));
+					//line.push(new Tile(j, i, "collision", "collisions"));
+				}
+			}
+		}
+		tileToModify.push(line);
+		WA.room.setTiles(line);
+	}
+	tic++;
+}
+
+function isNotInSafeZone(
+	height: number,
+	width: number,
+	numberTileLimit: number
+) {
+	const safeZoneCoordinates = {
+		heightMin: mapConfig.height / 2 - numberTileLimit,
+		heightMax: mapConfig.height / 2 + numberTileLimit,
+		widthMin: mapConfig.width / 2 - numberTileLimit,
+		widthMax: mapConfig.width / 2 + numberTileLimit,
+	};
+	return !(
+		height > safeZoneCoordinates.heightMin &&
+		height < safeZoneCoordinates.heightMax &&
+		width > safeZoneCoordinates.widthMin &&
+		width < safeZoneCoordinates.widthMax
+	);
 }
 
 export {};
