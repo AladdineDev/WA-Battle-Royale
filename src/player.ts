a80fcbdadd821740d877ac968d84e00db3801208
Aaldn <aaldn.dev@gmail.com>
2024-04-04 11:16:38 +0200

    feat: add updateLifePoint method

--- src/player.ts
+++ src/player.ts
@@ -1,4 +1,5 @@
 import { WorkadventurePlayerCommands } from "@workadventure/iframe-api-typings/front/Api/Iframe/player";
+import { callbackify } from "util";
 
 class Player {
   static async initPlayerVariables(
@@ -13,18 +14,30 @@ class Player {
     console.log(`Initialized Player: ${player}`);
   }
 
-  static async listenPlayerLife(
-    player: WorkadventurePlayerCommands
+  static async onLifePointEqualsZero(
+    player: WorkadventurePlayerCommands,
+    callback: Function
   ): Promise<void> {
     const subscription = await player.state
       .onVariableChange("lifePoint")
       .subscribe((lifePoint) => {
         if (lifePoint == 0) {
+          callback();
           console.log("Player is dead. Player Life Point: ", lifePoint);
           subscription.unsubscribe();
         }
       });
   }
+
+  static async updateLifePoint(
+    player: WorkadventurePlayerCommands,
+    lifePoint: number
+  ): Promise<void> {
+    WA.player.state.lifePoint = lifePoint
+    console.log(`Update Player Life Point: ${player}`);
+  }
+
+
 }
 
 export default Player;
