bf7fafd3f547a060cec5624d9c7629b748367a52
mtraore <mtraore@d-edge.com>
2024-04-04 16:18:55 +0200

    generate item on map

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import { Tile } from "./Entity/Tile";
+import {initTimerGame, GenerateItems} from "./Controller/GameController";
 
-console.log("Script started successfully");
+console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 let timeCounter = 300; // 5 minutes
@@ -21,8 +21,8 @@ WA.onInit()
 		console.log(map);
 		mapConfig.height = map.height ?? 0;
 		mapConfig.width = map.width ?? 0;
-
-		initTimerGame(timeCounter);
+        await GenerateItems(map);
+		initTimerGame(timeCounter, numberTileLimit, tic, mapConfig);
 
 		let movementConfig = true;
 		let playersConfig = true;
@@ -60,58 +60,6 @@ function closePopup() {
 		currentPopup.close();
 		currentPopup = undefined;
 	}
-}
-
-function initTimerGame(timeCounter: number) {
-	setInterval(() => {
-		timeCounter -= 10;
-		if (timeCounter <= 300) {
-			handleTileRemovedByTime();
-		}
-		numberTileLimit;
-	}, 10000); // 10 seconds
-}
-
-function handleTileRemovedByTime() {
-	const tileToModify = [];
-	for (let i = 0; i < mapConfig.height; i++) {
-		const line = [];
-		for (let j = 0; j < mapConfig.width; j++) {
-			if (
-				i == tic ||
-				i == mapConfig.height - tic ||
-				j == tic ||
-				j == mapConfig.width - tic
-			) {
-				if (isNotInSafeZone(i, j, numberTileLimit)) {
-					line.push(new Tile(j, i, "uglyBlue", "above/laptops"));
-					//line.push(new Tile(j, i, "collision", "collisions"));
-				}
-			}
-		}
-		tileToModify.push(line);
-		WA.room.setTiles(line);
-	}
-	tic++;
-}
-
-function isNotInSafeZone(
-	height: number,
-	width: number,
-	numberTileLimit: number
-) {
-	const safeZoneCoordinates = {
-		heightMin: mapConfig.height / 2 - numberTileLimit,
-		heightMax: mapConfig.height / 2 + numberTileLimit,
-		widthMin: mapConfig.width / 2 - numberTileLimit,
-		widthMax: mapConfig.width / 2 + numberTileLimit,
-	};
-	return !(
-		height > safeZoneCoordinates.heightMin &&
-		height < safeZoneCoordinates.heightMax &&
-		width > safeZoneCoordinates.widthMin &&
-		width < safeZoneCoordinates.widthMax
-	);
 }
 
 export {};
