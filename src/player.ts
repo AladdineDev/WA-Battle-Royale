7526ac7902cb775b5212d8822ec97694bcfd20cd
Aaldn <aaldn.dev@gmail.com>
2024-04-04 11:48:32 +0200

    feat: add inventory add and remove methods

--- src/player.ts
+++ src/player.ts
@@ -1,10 +1,21 @@
 import { WorkadventurePlayerCommands } from "@workadventure/iframe-api-typings/front/Api/Iframe/player";
+import Inventory from "./model/inventory";
+import Item from "./model/item";
 
 class Player {
   static async initPlayerVariables(
     player: WorkadventurePlayerCommands
   ): Promise<void> {
+
+    // Add LifePoint player variable
     await player.state.saveVariable("lifePoint", 3, {
+      public: false,
+      persist: false,
+      scope: "room",
+    });
+
+    // Add LifePoint player variable
+    await player.state.saveVariable("inventory", new Inventory(), {
       public: false,
       persist: false,
       scope: "room",
@@ -36,6 +47,29 @@ class Player {
     console.log(`Update Player Life Point: ${player}`);
   }
 
+  static async addItemToInventory(
+    player: WorkadventurePlayerCommands,
+    item: Item
+  ): Promise<void> {
+    var inventory = (player.state.inventory as Inventory)
+    WA.player.state.inventory = [...inventory.items, item] 
+    console.log(`Update Player Inventory: ${player}`);
+  }
+
+  static async removeItemToInventory(
+    player: WorkadventurePlayerCommands,
+    item: Item
+  ): Promise<void> {
+    const inventory = player.state.inventory as Inventory;
+    const items = inventory.items;
+    const index = items.findIndex(i => i.name === item.name);
+    if (index !== -1) {
+      inventory.items.splice(index, 1);
+      console.log(`Item removed from inventory: ${item.name}`);
+    } else {
+      console.log(`Item not found in inventory: ${item.name}`);
+    }
+  }
 
 }
 
